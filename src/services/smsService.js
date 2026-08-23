5617bf436822b9859037ff40f14e92fe6a14309e
Ritik Upadhyay <ritikupadhyay838@gmail.com>
2026-08-23 15:34:54 +0530

    New whatsap service added

--- src/services/smsService.js
+++ src/services/smsService.js
@@ -11,16 +11,14 @@ const sendSMS = async (mobileNumber, otp) => {
         const recipient = `91${mobileNumber}`;
 
         const response = await axios.post(
-            'https://api.tnstechnova.in/api/messages/send',
+            'https://official.technovicsolutions.com/api/v1/messages/send',
             {
-                device_id: Number(process.env.WHATSAPP_DEVICE_ID) || 1,
-                recipient,
-                message_type: 'text',
-                content: `Your FreshSabjiHub verification code is: *${otp}*\n\nThis code will expire in 5 minutes.\n\nDo not share this code with anyone.`
+                phone: recipient,
+                message: `Your FreshSabjiHub verification code is: *${otp}*\n\nThis code will expire in 5 minutes.\n\nDo not share this code with anyone.`
             },
             {
                 headers: {
-                    'X-API-Key': process.env.WHATSAPP_API_KEY,
+                    'X-API-Key': '8701b828ee4143b76d32fd7136a5c3b645bc3ea54fc34dad6af2a918d81e514a',
                     'Content-Type': 'application/json'
                 }
             }
